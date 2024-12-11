9f04cfb309f00e5a2b53651c1d853ce7e043048e
XrayTH <143976649+XrayTH@users.noreply.github.com>
2024-12-11 12:27:23 -0500

    v11.3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const https = require('https'); 
+const fs = require('fs');       
 const sequelize = require('./config/db');
 require('dotenv').config();
 
@@ -9,8 +11,8 @@ const programaRoutes = require('./routes/programaRoutes');
 const intructorRoutes = require('./routes/instructorRoutes');
 const coordinadorRoutes = require('./routes/coordinadorRoutes');
 const jornadaRoutes = require('./routes/jornadaRoutes');
-const fichaRoutes = require('./routes/fichaRoutes');
-const asignacionRoutes = require('./routes/asignacionRoutes');
+fichaRoutes = require('./routes/fichaRoutes');
+asignacionRoutes = require('./routes/asignacionRoutes');
 const emailMasivoRoutes = require('./routes/emailMasivoRoutes');
 const emailRoutes = require('./routes/emailRoutes');
 const ambienteRoutes = require('./routes/ambienteRoutes');
@@ -19,22 +21,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const IP = process.env.IP || "0.0.0.0"
+let credentials = null;
+try {
+  const privateKey = fs.readFileSync('/etc/ssl_httpd/private/httpd.key', 'utf8');
+  const certificate = fs.readFileSync('/etc/ssl_httpd/certs/httpd.crt', 'utf8');
+  credentials = { key: privateKey, cert: certificate };
+} catch (error) {
+  console.warn('Certificados no encontrados o inválidos. El servidor funcionará en HTTP.');
+}
+
 const PORT = process.env.PORT || 3000;
-
-const verifySameOrigin = (req, res, next) => {
-  const allowedHost = `${IP}:${PORT}`; 
-
-  const host = req.get('Host');
-  
-  if (!host || host !== allowedHost) {
-    return res.status(403).json({ message: 'Acceso denegado: Solo solicitudes internas permitidas' });
-  }
-
-  next();
-};
-
-app.use('/fichaSenaService', verifySameOrigin);
 
 app.use('/fichaSenaService', usuarioRoutes);
 app.use('/fichaSenaService', programaRoutes);
@@ -46,6 +42,10 @@ app.use('/fichaSenaService', asignacionRoutes);
 app.use('/fichaSenaService', emailMasivoRoutes);
 app.use('/fichaSenaService', emailRoutes);
 app.use('/fichaSenaService', ambienteRoutes);
+
+app.get('/fichaSenaService/status', (req, res) => {
+  res.status(200).json({ status: 'El backend está funcionando correctamente' });
+});
 
 const buildPath = path.join(__dirname, 'build');
 app.use(express.static(buildPath));
@@ -59,9 +59,15 @@ const startServer = async () => {
     await sequelize.authenticate();
     console.log('Conexión a la base de datos establecida.');
 
-    app.listen(PORT, '0.0.0.0', () => {
-      console.log(`Servidor escuchando en el puerto ${PORT}`);
-    });
+    if (credentials) {
+      https.createServer(credentials, app).listen(443, '0.0.0.0', () => {
+        console.log('Servidor HTTPS escuchando en el puerto 443');
+      });
+    } else {
+      app.listen(PORT, '0.0.0.0', () => {
+        console.log(`Servidor HTTP escuchando en el puerto ${PORT}`);
+      });
+    }
   } catch (error) {
     console.error('Error al conectar a la base de datos:', error);
   }
